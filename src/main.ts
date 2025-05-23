bcccc10bf881643886fc69b372d43b1fc9320383
alberto-ifd <alberto.jolyt@ifd.com.mx>
2025-05-22 21:05:23 -0600

    Sentence change and environment variable fixed

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (process.env.CLIENT_UR ?? 'http://localhost:3000'),
+    origin: (process.env.CLIENT_URL || 'http://localhost:3000'),
     credentials: true,
   });
 
